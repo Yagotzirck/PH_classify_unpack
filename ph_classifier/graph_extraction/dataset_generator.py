b05e6a34227166aaaf494396a2945f4b4ae5d0eb
Alessio "Yagotzirck" Simbula <yagotzirck@gmail.com>
2025-08-18 13:30:47 +0200

    Updated dataset_generator: packer label in 1st part of file name, instead of using a json file

--- ph_classifier/graph_extraction/dataset_generator.py
+++ ph_classifier/graph_extraction/dataset_generator.py
@@ -16,11 +16,11 @@ import sys
 import json
 import radare2_extract_custom_gcg as r2_gcg
 
+
 DATASET_SAMPLES_PATH = "../dataset/samples"
 GENERATED_GRAPHS_BASE_PATH = "../dataset/graphs"
-SAMPLES_LABELS_PATH = "../dataset/samples_labels.json"
-
 PACKED_FILES = glob.iglob(f"{DATASET_SAMPLES_PATH}/*")
+
 
 if '--radare2' in sys.argv:
     gcg = r2_gcg
@@ -32,15 +32,12 @@ else:
     raise UnspecifiedCallGraphGeneratorError()
 
 GENERATED_GRAPHS_PATH = os.path.join(GENERATED_GRAPHS_BASE_PATH, cg_extractor)
-if not os.path.exists(GENERATED_GRAPHS_PATH):
-    os.makedirs(GENERATED_GRAPHS_PATH)
+os.makedirs(GENERATED_GRAPHS_PATH, exist_ok=True)
 
 GENERATED_GRAPHVIZ_PATH = os.path.join(
     GENERATED_GRAPHS_BASE_PATH, f'{cg_extractor}_graphviz'
 )
-if not os.path.exists(GENERATED_GRAPHVIZ_PATH):
-    os.makedirs(GENERATED_GRAPHVIZ_PATH)
-
+os.makedirs(GENERATED_GRAPHVIZ_PATH, exist_ok=True)
 
 discarded_list = []
 
@@ -79,30 +76,6 @@ for curr_file in PACKED_FILES:
             )
 
 print("\nDiscarded files: ", discarded_list)
-
-
-# Update the labels, removing the discarded files
-with open(SAMPLES_LABELS_PATH, 'r') as in_fp:
-    samples_labels = json.load(in_fp)
-
-discarded_set = set(discarded_list)
-
-for samples_list in samples_labels.values():
-    for sample in samples_list:
-        if sample in discarded_set:
-            samples_list.remove(sample)
-
-samples_labels_basename_no_ext, _ = os.path.splitext(
-    os.path.basename(SAMPLES_LABELS_PATH)
-)
-
-out_samples_labels_filename = os.path.join(
-    GENERATED_GRAPHS_BASE_PATH,
-    f'{samples_labels_basename_no_ext}_{cg_extractor}.json'
-)
-
-with open(out_samples_labels_filename, 'w') as out_fp:
-    json.dump(samples_labels, out_fp)
 
 
 # Save the list of discarded samples
